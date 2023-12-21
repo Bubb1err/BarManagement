f1707c1646a121d1c4f1875aa3ce6da13dd0624b
Valeria Timchenko <timcenkovaleria79@gmail.com>
2023-12-22 00:19:01 +0200

    Add project files.

---
+++ src/BarManagment.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BarManagment.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
