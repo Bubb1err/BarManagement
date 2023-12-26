ddcbc46adcbf8ebee28b554f6b859e5fec57e439
Valeria Timchenko <timcenkovaleria79@gmail.com>
2023-12-26 23:41:35 +0200

    base entity

---
+++ src/Core/BarManagment.Domain/Entities/Base/Entity.cs
@@ -0,0 +1,45 @@
+﻿namespace BarManagment.Domain.Entities.Base
+{
+    public abstract class Entity : IEquatable<Entity>
+    {
+        public Guid Id { get; private init; }
+
+        protected Entity(Guid id)
+        {
+            Id = id;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            
+            if (obj.GetType() != GetType()) return false;
+
+            if (obj is not Entity entity) return false;
+
+            return entity.Id == Id;
+        }
+        public static bool operator ==(Entity? first, Entity? second)
+        {
+            return first is not null && second is not null && first.Equals(second);
+        }
+        public static bool operator !=(Entity? first, Entity? second)
+        {
+            return !(first == second);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode() * 37;
+        }
+
+        public bool Equals(Entity? other)
+        {
+            if (other == null) return false;
+
+            if (other.GetType() != GetType()) return false;
+
+            return other.Id == Id;
+        }
+    }
+}
